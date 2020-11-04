c77157cce404244f5af51677b930783ba66bfa39
Antonio Salsi <passy.linux@zresa.it>
2020-11-04 22:14:29 +0100

    yp

--- src/components/Pages/Page.jsx
+++ src/components/Pages/Page.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
 
 export default class Pages extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            iconWidth : 0
+        };
+        this.icon = React.createRef();
+    }
+
     render = () => 
     <div>
-        <i className={`icon fab ${this.props.icon}`} xs={1}></i>
-        <h2>{this.props.title}</h2>
-        <p className="descr">{this.props.descr}</p>
-    </div>;
+        <i ref={this.icon}  className={`icon rounded-circle float-left fas ${this.props.icon}`}></i>
+        <h2 className="float-left clearfix" style={{width: `${this.props.containerWidth - this.icon.current.state.width}px`}>{this.props.title}</h2>
+        <p className="clearfix w-100">{this.props.descr}</p>
+        {this.props.containerWidth}
+    </div>
 } 
--- src/components/Pages/Pages.jsx
+++ src/components/Pages/Pages.jsx
@@ -6,13 +6,40 @@ import PageService from '../../services/PageService';
 export default class Pages extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {pages: PageService.pages};
+        this.state = {
+            pages: PageService.pages,
+            containerWidth : 0
+        };
+        this.container = React.createRef();
     }
 
+    componentWillUnmount() {
+        window.removeEventListener('resize', () => {});
+    }
+    componentDidMount() {
+        window.addEventListener('resize', () => 
+            this.setState({containerWidth: this.container.current.offsetWidth})
+        );
+    }
+
+
     render = () => 
-    <div id="pages">
-            {
-                this.state.pages.map ((page, index) => <Page key={page.id} index={index} icon={page.icon} title={page.title} descr={page.descr} />)
-            }
-    </div>;
+        <div 
+            id="pages" 
+            ref={this.container} 
+        >
+                {
+                    this.state.pages.map (
+                        (page, index) => 
+                            <Page 
+                                key={page.id} 
+                                containerWidth={this.state.containerWidth} 
+                                index={index} 
+                                icon={page.icon} 
+                                title={page.title} 
+                                descr={page.descr} 
+                            />
+                    )
+                }
+        </div>;
 } 
